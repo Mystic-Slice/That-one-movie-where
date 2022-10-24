2f71a1efb9aa3c64fd98fcf59fe1ed4c8048de00
Mystic-Slice <vaashwath@gmail.com>
2022-10-25 00:55:10 +0530

    OMDb API with almost all details but with virtually no limit for requests (https://www.omdbapi.com/)

--- src/api.rs
+++ src/api.rs
@@ -2,16 +2,13 @@ use crate::{movie::Movie, movie_error::MovieError};
 
 pub async fn get_movie_info(id: &str) -> Result<Movie, MovieError> {
 
-    const API_URL: &str = "https://movie-details1.p.rapidapi.com/imdb_api/movie";
+    const API_URL: &str = "http://www.omdbapi.com/";
     const API_KEY: &str = "404_API_KEY_MISSING";
-    const API_HOST: &str = "movie-details1.p.rapidapi.com";
 
     let client = reqwest::Client::new();
 
     let response = client.get(API_URL)
-        .query(&[("id", id)])
-        .header("X-RapidAPI-Key", API_KEY)
-        .header("X-RapidAPI-Host", API_HOST)
+        .query(&[("apikey", API_KEY), ("i", id), ("plot", "full")])
         .send().await;
     
     if let Err(error) = response {
@@ -32,20 +29,22 @@ pub async fn get_movie_info(id: &str) -> Result<Movie, MovieError> {
 
     let json_response = json_response.unwrap();
 
-    let req_keys = vec!["title", "rating", "image", "imdb_type"];
+    let req_keys = vec!["Title", "imdbRating", "Poster", "Type"];
     for key in req_keys {
         if !json_response.has_key(key) {
+            return Err(MovieError::new(&format!("Could not get the complete info about the movie with id {id}")))
+        } else if json_response[key] == "N/A" {
             return Err(MovieError::new(&format!("Could not get the complete info about the movie with id {id}")))
         }
     }
 
-    if json_response["imdb_type"].to_string() != "movie" {
+    if json_response["Type"].to_string() != "movie" {
         return Err(MovieError::new(&format!("{} is not a movie", json_response["title"].to_string())))
     }
 
     Ok(Movie::new(
-        json_response["title"].to_string(),
-        json_response["rating"].to_string().parse().unwrap(),
-        json_response["image"].to_string(),
+        json_response["Title"].to_string(),
+        json_response["imdbRating"].to_string().parse().unwrap(),
+        json_response["Poster"].to_string(),
     ))
 }
